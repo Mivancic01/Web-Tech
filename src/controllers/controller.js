cdda33d1fde9201b0b0832e3d2c517166747c7ce
PoojaYeli <puja.v.y@gmail.com>
2021-01-19 19:58:02 +0100

    changes

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -12,11 +12,15 @@ import {
   addUser,
   updateUser,
   updateFriendByName,
-  fb,
   addNotes,
   readNotes,
   updateNotes,
-  deleteNotes
+  deleteNotes,
+  toggleNote,
+  loggedUser,
+  userLoggedOut,
+  userSingUp,
+  userSingIn
 } from '../service/firebase.service';
 
 export class Controller {
@@ -46,91 +50,142 @@ export class Controller {
   }
 
   handleSignedinUser = () => {
-    var user = fb.auth().currentUser;
-    if (user) {
-      // User is signed in.
-      this.view.bindSignoutUnsuccess();
-    } else {
-      // No user is signed in.
-      fb.auth().signOut()
-      .catch((error) => {
-            // An error happened.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log("User signed out error");
-            console.log(errorMessage);
-            this.view.bindSignoutError(errorMessage);
-          });
-      this.view.bindSignoutSuccess();
-    }
+  console.log("loggedUser");
+//  console.log(loggedUser);
+    loggedUser((res) => {
+        if(res.success){
+          // User is signed in.
+          this.view.bindSignoutUnsuccess();
+        }
+        else{
+            this.handleSignoutUser();
+            this.view.bindSignoutSuccess();
+        }
+    });
+//    if (loggedUser) {
+//      // User is signed in.
+//      this.view.bindSignoutUnsuccess();
+//
+//    } else {
+//      // No user is signed in.
+////      fb.auth().signOut()
+////      .catch((error) => {
+////            // An error happened.
+////            var errorCode = error.code;
+////            var errorMessage = error.message;
+////            console.log("User signed out error");
+////            console.log(errorMessage);
+////            this.view.bindSignoutError(errorMessage);
+////          });
+//      this.view.bindSignoutSuccess();
+//    }
   }
 
   handleSignoutUser = () => {
-  var userLoggedout = false;
-    fb.auth().signOut().
-    then(() => {
-      // Sign-out successful.
-      console.log("User signed out success");
-      this.view.bindSignoutSuccess();
-      this.model.readAllNotes();
-      this.onNoteListChanged(this.model.notes);
-    }).catch((error) => {
-      // An error happened.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log("User signed out error");
-      console.log(errorMessage);
-      this.view.bindSignoutError(errorMessage);
-    });
+//  var userLoggedout = false;
+//    fb.auth().signOut().
+    userLoggedOut((res)=>{
+        if(res.success){
+          this.view.bindSignoutSuccess();
+          this.model.readAllNotes();
+          this.onNoteListChanged(this.model.notes);
+        }
+        else{
+            this.view.displaySigninError(res.message);
+        }
+      });
+//    then(() => {
+//      // Sign-out successful.
+//      console.log("User signed out success");
+//      this.view.bindSignoutSuccess();
+//      this.model.readAllNotes();
+//      this.onNoteListChanged(this.model.notes);
+//    }).catch((error) => {
+//      // An error happened.
+//      var errorCode = error.code;
+//    var errorMessage = error.message;
+//    console.log("User signed out error");
+//    console.log(errorMessage);
+//    this.view.bindSignoutError(errorMessage);
+//    });
 
   }
 
   handleSigninSubmit = (email, password) => {
   console.log("signin");
-    fb.auth().signInWithEmailAndPassword(email, password)
-//    fb.auth().createUserWithEmailAndPassword(email, password)
-    .then((user) => {
-      // Signed in
-      console.log("User signed in");
-      console.log(user);
-//      this.handleSignedinUser();
+  userSingIn(email, password, (res)=>{
+    if(res.success){
         this.view.userSignedin(email);
         readNotes(email, (notesFromDB) => {
             console.log(notesFromDB);
             this.view.displayNotes(notesFromDB);
             this.model.notes = notesFromDB;
         });
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log("Error");
-      console.log(errorMessage);
-      this.view.displaySigninError(errorMessage);
-    });
+    }
+    else{
+        this.view.displaySigninError(res.message);
+    }
+  });
+//    fb.auth().signInWithEmailAndPassword(email, password)
+////    fb.auth().createUserWithEmailAndPassword(email, password)
+//    .then((user) => {
+//      // Signed in
+//      console.log("User signed in");
+//      console.log(user);
+////      this.handleSignedinUser();
+//        this.view.userSignedin(email);
+//        readNotes(email, (notesFromDB) => {
+//            console.log(notesFromDB);
+//            this.view.displayNotes(notesFromDB);
+//            this.model.notes = notesFromDB;
+//        });
+//    })
+//    .catch((error) => {
+//      var errorCode = error.code;
+//      var errorMessage = error.message;
+//      console.log("Error");
+//      console.log(errorMessage);
+//      this.view.displaySigninError(errorMessage);
+//    });
   }
 
   handleSignupSubmit = (name, secondName, email, password) => {
     console.log("signup");
 
 //    addNotes(email, "noteTitle", "noteText", "red" );
-    fb.auth().createUserWithEmailAndPassword(email, password)
-    .then((user) => {
-      // Signed in
-      addUser(name, secondName, "",email);
-      console.log("User signed in");
-      console.log(user);
-      this.view.userSignedin(email);
-      this.view.displayNotes([]);
-      this.model.notes = [];
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log("Error");
-      console.log(errorMessage);
-      this.view.displaySigninError(errorMessage);
+    userSingUp(name, secondName, email, password, (res)=>{
+        if(res.success){
+//          userSingIn(email, password, (res1)=>{
+//          if(res1.success){
+              this.view.userSignedin(email);
+            this.view.displayNotes([]);
+            this.model.notes = [];
+//          }
+//          else{
+//              this.view.displaySigninError(res1.message);
+//          }
+//        });
+        }
+        else{
+              this.view.displaySigninError(res.message);
+          }
     });
+//    .then((user) => {
+      // Signed in
+//      addUser(name, secondName, "",email);
+//      console.log("User signed in");
+//      console.log(user);
+//      this.view.userSignedin(email);
+//      this.view.displayNotes([]);
+//      this.model.notes = [];
+//    })
+//    .catch((error) => {
+//      var errorCode = error.code;
+//      var errorMessage = error.message;
+//      console.log("Error");
+//      console.log(errorMessage);
+//      this.view.displaySigninError(errorMessage);
+//    });
   }
 
   onNoteListChanged = (notes) => {
@@ -148,72 +203,92 @@ export class Controller {
     //updateUser(noteTitle, noteText, noteColor, "bla")
     //updateFriendByName("noteTitle", "momo", "laaaaaaa", "email@asd")
     //addFriend("noteTitle", "novi", "meme", "email")
-    var user = fb.auth().currentUser;
+//    var user = fb.auth().currentUser;
     const worker = new Worker("../src/worker.js");
     worker.postMessage( noteText);
-    if (user) {
-      // User is signed in.
-      worker.onmessage = e => {
-        var encodedText = e.data;
-        const nextNoteId = this.model.notes.length > 0 ? this.model.notes[this.model.notes.length - 1].id + 1 : 1;
-        addNotes(nextNoteId, user.email, noteTitle, encodedText, noteColor, image );
-        readNotes(user.email, (notesFromDB) => {
-          console.log(notesFromDB);
-          this.view.displayNotes(notesFromDB);
-          this.model.notes = notesFromDB;
-        });
-      };
-    } else {
-      // No user is signed in.
-      worker.onmessage = e => {
-      var encodedText = e.data;
-      this.model.addNote(noteTitle, encodedText, noteColor, image);
-    };
-    }
+    loggedUser((res) => {
+        if(res.success){
+          // User is signed in.
+          worker.onmessage = e => {
+              var encodedText = e.data;
+              const nextNoteId = this.model.notes.length > 0 ? this.model.notes[this.model.notes.length - 1].id + 1 : 1;
+              addNotes(nextNoteId, res.user.email, noteTitle, encodedText, noteColor, image);
+              readNotes(res.user.email, (notesFromDB) => {
+                console.log(notesFromDB);
+                this.view.displayNotes(notesFromDB);
+                this.model.notes = notesFromDB;
+              });
+            };
+        }
+        else{
+            worker.onmessage = e => {
+              var encodedText = e.data;
+              this.model.addNote(noteTitle, encodedText, noteColor, image);
+            };
+        }
+    });
   }
 
   handleEditNote = (id, todoText) => {
-  var user = fb.auth().currentUser;
-  const worker = new Worker("../src/worker.js");
-  worker.postMessage(todoText);
-      if (user) {
-        // User is signed in.
-        worker.onmessage = e => {
-          var encodedText = e.data;
-          updateNotes(id, user.email, encodedText );
-          readNotes(user.email, (notesFromDB) => {
-              console.log(notesFromDB);
-              this.view.displayNotes(notesFromDB);
-              this.model.notes = notesFromDB;
-          });
-        };
-      } else {
-        // No user is signed in.
-        worker.onmessage = e => {
-          var encodedText = e.data;
-          this.model.editNote(id, encodedText);
-        };
-      }
+      const worker = new Worker("../src/worker.js");
+      worker.postMessage(todoText);
+      loggedUser((res) => {
+          if(res.success){
+              // User is signed in.
+              worker.onmessage = e => {
+                var encodedText = e.data;
+                updateNotes(id, res.user.email, encodedText );
+                readNotes(res.user.email, (notesFromDB) => {
+                    console.log(notesFromDB);
+                    this.view.displayNotes(notesFromDB);
+                    this.model.notes = notesFromDB;
+                });
+              };
+          }
+          else{
+            // No user is signed in.
+            worker.onmessage = e => {
+              var encodedText = e.data;
+              this.model.editNote(id, encodedText);
+            };
+          }
+      });
   }
 
   handleDeleteNote = (id) => {
-    var user = fb.auth().currentUser;
-      if (user) {
+    loggedUser((res) => {
+      if(res.success){
         // User is signed in.
-        deleteNotes(id, user.email );
-        readNotes(user.email, (notesFromDB) => {
+        deleteNotes(id, res.user.email );
+        readNotes(res.user.email, (notesFromDB) => {
             console.log(notesFromDB);
             this.view.displayNotes(notesFromDB);
             this.model.notes = notesFromDB;
         });
-      } else {
-        // No user is signed in.
-       this.model.deleteNote(id);
       }
+      else{
+        // No user is signed in.
+        this.model.deleteNote(id);
+      }
+    });
   }
 
   handleToggleNote = (id) => {
-    this.model.toggleNote(id);
+    loggedUser((res) => {
+      if(res.success){
+        // User is signed in.
+        toggleNote(id, res.user.email);
+        readNotes(res.user.email, (notesFromDB) => {
+            console.log(notesFromDB);
+            this.view.displayNotes(notesFromDB);
+            this.model.notes = notesFromDB;
+        });
+      }
+      else{
+        // No user is signed in.
+        this.model.toggleNote(id);
+      }
+  });
   }
 
 }
